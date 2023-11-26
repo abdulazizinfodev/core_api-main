21332d786f72cb0ced6d346b5073e9853d2be7b9
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-11-26 08:09:32 +0500

    Update serializer.py

--- apps/video/serializer.py
+++ apps/video/serializer.py
@@ -9,16 +9,32 @@ from apps.users.serializers import UserReadSerializer
 #         fields = ('id', 'name',)
 
 
+# class ModulSerializer(serializers.ModelSerializer):
+#     all_videos = serializers.SerializerMethodField()
+
+#     class Meta:
+#         model = ModulClass
+#         fields = ('id', 'name', 'all_videos',)
+
+#     def get_all_videos(self, obj):
+#         module_videos = VideoApp.objects.filter(modul=obj)
+#         return [video.name for video in module_videos]
+
+
+class VideoAppSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VideoApp
+        fields = ('name', 'video', 'description',
+                  'comment', 'created_at', 'marked_view')
+
+
 class ModulSerializer(serializers.ModelSerializer):
-    all_videos = serializers.SerializerMethodField()
+    videos = VideoAppSerializer(
+        source='videoapp_set', many=True, read_only=True)
 
     class Meta:
         model = ModulClass
-        fields = ('id', 'name', 'all_videos',)
-
-    def get_all_videos(self, obj):
-        module_videos = VideoApp.objects.filter(modul=obj)
-        return [video.name for video in module_videos]
+        fields = ('id', 'name', 'videos', 'created_at')
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -29,11 +45,11 @@ class CommentSerializer(serializers.ModelSerializer):
         fields = ('id', 'created_by', 'comment',)
 
 
-class VideoAppSerializer(serializers.ModelSerializer):
-    modul = ModulSerializer(read_only=True)
-    comment = CommentSerializer()
+# class VideoAppSerializer(serializers.ModelSerializer):
+#     modul = ModulSerializer(read_only=True)
+#     comment = CommentSerializer()
 
-    class Meta:
-        model = VideoApp
-        fields = ('id', 'modul', 'name',
-                  'description', 'marked_view', 'comment', 'video', 'get_thumbnail',)
+#     class Meta:
+#         model = VideoApp
+#         fields = ('id', 'modul', 'name',
+#                   'description', 'marked_view', 'comment', 'video', 'get_thumbnail',)
