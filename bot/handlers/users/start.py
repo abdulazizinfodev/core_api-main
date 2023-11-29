e5ff176b84adb26a77234b3cd16d781d3faf5496
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-11-29 23:59:52 +0500

    adds

--- bot/handlers/users/start.py
+++ bot/handlers/users/start.py
@@ -9,9 +9,41 @@ import json
 async def show_menu1(message: Message):
     usr = save_user(message)
     if usr.status_code == 201:
-        await message.reply(f"<b>Xolat:</b> hisobingiz mafaqqiyatli yaratildi.")
+        text_info = """
+Assalomu alaykum! NOXONFX UZ botga xush kelibsiz!
+Keling sizni xizmatlarimiz bilan tanishtiraman.
+
+Product va Servislar
+
+Product va Servislarimiz haqida batafsil ma'lumot olish uchun pastdagi havolani bosing 👇
+
+https://www.noxonfx.com/
+
+Bog'lanish
+
+Toshkent Filial: +998 95 9 80 51 11
+Farg'ona  Filial: +998 95 016 69 68
+Toshkent   ICT: +998 90 965 69 96
+
+⚠️⚠️⚠️
+Tariflar uchun to'lov qilib adminga telegram username, screenshot (chek)ni adminlarimizdan biriga yuboring!
+⚠️⚠️⚠️
+
+@noxonfx_admin
+
+Taklif va Murojaat uchun adminga quyidagi 
+forma shaklida murojat qiling:
+
+👇 Iltimos Formani to'ldiring 👇
+
+Ism va Familya:
+Telegram username:
+Telefon nomer:
+Taklif yoki Murojaatingiz:
+"""
+        await message.reply({text_info})
     else:
-        await message.reply(f"<b>Xolat:</b> ushbu hisob allaqachon yaratilgan.")
+        await message.reply(f"Ushbu hisob allaqachon yaratilgan. \n\n{text_info}")
 
 
 @dp.message_handler(Command('kirish'))
