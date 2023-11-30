19962f1501d3e7a042982d26641cd6ce732afd6c
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-11-30 10:28:26 +0500

    adds

--- core/settings.py
+++ core/settings.py
@@ -89,13 +89,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'myproject',
+            'USER': 'myprojectuser',
+            'PASSWORD': 'password',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 # POSTGRES_DB = os.environ.get("POSTGRES_DB")
 # POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
