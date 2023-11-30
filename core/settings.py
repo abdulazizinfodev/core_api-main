4ba877052cc04885905c116b003923264a7074b9
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-11-30 08:20:07 +0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -101,7 +101,7 @@ POSTGRES_DB = os.environ.get("POSTGRES_DB")
 POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
 POSTGRES_USER = os.environ.get("POSTGRES_USER")
 POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
-POSTGRES_PORT = '*'
+POSTGRES_PORT = '8000'
 
 DATABASES = {
     "default": {
